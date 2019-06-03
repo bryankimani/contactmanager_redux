8d06b3d2748b0fe28038be675a50657885451a5a
brnkimani <brnkimani@gmail.com>
2019-06-03 08:05:43 +0300

    Add redux reducers

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,39 @@
+import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT, GET_CONTACT, UPDATE_CONTACT  } from '../actions/types';
+
+const initiateState = {
+    contacts: [],
+    contact: {}
+};
+
+export default function(state = initiateState, action){
+    switch(action.type){
+        case GET_CONTACTS:
+            return {
+                ...state,
+                contacts: action.payload
+            };
+        case GET_CONTACT:
+            return {
+                ...state,
+                contact:  action.payload
+            }; 
+        case DELETE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.filter(contact => contact.id !== action.payload)
+            };    
+        case ADD_CONTACT:
+            return {
+                ...state,
+                contacts:[action.payload, ...state.contacts]
+            };   
+        case UPDATE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.map(contact => 
+                    contact.id === action.payload.id ? (contact = action.payload) : contact)
+            };
+        default:
+            return state;
+    }
+}
